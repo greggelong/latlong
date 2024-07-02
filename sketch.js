ff25eaae73e32c13c9012c5f00240d9681236d1b
Greg K <gkreisman_cep@qq.com>
2024-07-02 15:58:09 +0800

    add workers back in

--- sketch.js
+++ sketch.js
@@ -154,8 +154,8 @@ async function getStreet(sitee) {
   print(loc)
   //img = await createImg("https://image.pollinations.ai/prompt/a%20view%20from%20the%20back%20of%20a%20taxi%20in%20a%20futuristic%20"+loc+"%20with%20a%20cool%20person%20in%20the%20backseat?width=800&height=800&nologo=true&seed="+floor(random(1026)),"street scene")
 
-  //img = await createImg("https://image.pollinations.ai/prompt/two%20male%20workers%20and%20a%20female%20worker%20with%20yellow%20helments%20working%20on%20a%20larg%20construction%20site%20in%20"+loc+"?width=800&height=800&nologo=true&seed="+floor(random(1026)),"street scene");
-  img = await createImg("https://image.pollinations.ai/prompt/a%20larg%20construction%20site%20in%20"+loc+"?width=800&height=800&nologo=true&seed="+floor(random(1026)),"street scene");
+    img = await createImg("https://image.pollinations.ai/prompt/two%20male%20workers%20and%20a%20female%20worker%20with%20yellow%20helments%20working%20on%20a%20larg%20construction%20site%20in%20"+loc+"?width=800&height=800&nologo=true&seed="+floor(random(1026)),"street scene");
+  //img = await createImg("https://image.pollinations.ai/prompt/a%20larg%20construction%20site%20in%20"+loc+"?width=800&height=800&nologo=true&seed="+floor(random(1026)),"street scene");
   // let prmpts = [
   //   "https://image.pollinations.ai/prompt/%20workers%20from%20all%20around%20the%20world%20come%20together%20on%20the%20worksite,%20they%20cheer%20and%20are%20joyous%20as%20fireworks%20go%20off%20overhead?width=800&height=800&nologo=true&seed=",
   //   "https://image.pollinations.ai/prompt/%20workers%20from%20around%20the%20world%20unite,%20each%20one%20with%20a%20flag%20in%20hand%20and%20big%20red%20banner%20which%20reads%20%E2%80%9Cworkers%20of%20the%20world%20unite%E2%80%9D%20on%20a%20construction%20site%20?width=800&height=800&nologo=true&seed=",
